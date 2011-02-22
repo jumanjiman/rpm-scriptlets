65bec3044fdfb3e6530f364c2b309fb4a57a9a91
Paul Morgan <jumanjiman@gmail.com>
2011-02-22 14:36:48 -0500

    improved demo logic and reporting

--- demo.sh
+++ demo.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# purpose: demonstrate broken yum/rpm behavior 
-#          with respect to scriptlets (pre, post, preun, postun)
-#
-# usage:   update the following three variables as appropriate
-#          for your test box.
-
-dir=/tmp/tito/noarch/
+dir=/tmp/tito/noarch
 rpm1=$dir/rpm-scriptlets-0.1-1.fc14.noarch.rpm
 rpm2=$dir/rpm-scriptlets-0.2-1.fc14.noarch.rpm
+yum_options="-y --nogpgcheck"
 
 # ensure we do not have rpm-scriptlets installed
 yum -y remove rpm-scriptlets &> /dev/null
@@ -16,25 +11,52 @@ yum -y remove rpm-scriptlets &> /dev/null
 # marker for grep
 marker='>>>>>>>>>>'
 
-echo "Demo with yum"
-echo " ============="
+run_command() {
+  _cmd="$*"
+  output="$($_cmd 2>&1)"
+  _rc=$?
+  if [[ $_rc -eq 0 ]]; then
+    IFS=" " grep -Eoh "${marker}.*$" <<< $output
+  else
+    (
+    echo "error:"
+    echo $output 
+    ) >&2
+  fi
+  return $?
+}
+
+echo "Demo with yum and plugins"
+echo "========================="
 echo "initial install"
-yum -y --nogpgcheck localinstall $rpm1 2>&1 | grep "$marker"
+run_command "yum $yum_options localinstall $rpm1"
 
 echo "upgrade"
-yum -y --nogpgcheck localinstall $rpm2 2>&1 | grep "$marker"
+run_command "yum $yum_options localinstall $rpm2"
 
 echo "final removal"
-yum -y remove rpm-scriptlets 2>&1 | grep "$marker"
+run_command "yum $yum_options remove rpm-scriptlets"
+
+echo
+echo "Demo with yum and NO plugins"
+echo "============================"
+echo "initial install"
+run_command "yum $yum_options --disableplugin=* localinstall $rpm1"
+
+echo "upgrade"
+run_command "yum $yum_options --disableplugin=* localinstall $rpm2"
+
+echo "final removal"
+run_command "yum $yum_options --disableplugin=* remove rpm-scriptlets"
 
 echo
 echo "Demo with rpm"
-echo " ============="
+echo "============="
 echo "initial install"
-rpm -U $rpm1 2>&1 | grep "$marker"
+run_command "rpm -U $rpm1"
 
 echo "upgrade"
-rpm -U $rpm2 2>&1 | grep "$marker"
+run_command "rpm -U $rpm2"
 
 echo "final removal"
-rpm -e 2>&1 | grep "$marker"
+run_command "rpm -e rpm-scriptlets"
